7cb189971159e431ea57a8339c0abe2b91cc4b19
chaitu143123 <bommisettychaitanya3@gmail.com>
2021-10-22 15:58:29 +0530

    view cart

---
+++ galaxy-store-angular/src/app/add-view-cart/add-view-cart.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-view-cart',
+  templateUrl: './add-view-cart.component.html',
+  styleUrls: ['./add-view-cart.component.css']
+})
+export class AddViewCartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
